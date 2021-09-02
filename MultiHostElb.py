c8ee2f0dee99dea022d4c38afbcd738f985e4ec2
Dan Boitnott <dboitnot@gmail.com>
2021-09-02 16:21:05 -0500

    model written and tested

--- MultiHostElb.py
+++ MultiHostElb.py
@@ -35,6 +35,13 @@ def model_string_or_list(key, values):
             values[key] = [v]
     return values
 
+
+def model_limit_values(allowed, v):
+    if v not in allowed:
+        raise ValueError("value must be one of {}", allowed)
+    return v
+
+
 #
 # IMPORTANT: The following classes are DATA CLASSES using pydantic.
 #            DO NOT add behavior to them beyond input validation. Use functions
@@ -64,6 +71,129 @@ class RuleModel(BaseModel):
         if len(values['hosts']) < 1 and len(values['paths']) < 1:
             raise ValueError('one of hosts or paths must be specified')
         return values
+
+
+class RedirectModel(BaseModel):
+    host: Optional[str]
+    path: Optional[str]
+    port: Optional[int]
+    protocol: Optional[str]
+    query: Optional[str]
+    status_code = 302
+
+    @validator('protocol')
+    def check_protocol(cls, v):
+        return model_limit_values(['HTTP', 'HTTPS', '#{protocol}'], v)
+
+    @validator('status_code')
+    def check_status_code(cls, v):
+        return model_limit_values([301, 302], v)
+
+
+class TargetModel(BaseModel):
+    id: str
+    port: Optional[int]
+
+
+class HealthCheckModel(BaseModel):
+    interval_seconds: Optional[int]
+    path: Optional[str]
+    timeout_seconds: Optional[int]
+
+
+class FixedResponseModel(BaseModel):
+    message_body: Optional[str]
+    content_type = 'text/plain'
+    status_code = 200
+
+    @validator('content_type')
+    def check_content_type(cls, v):
+        return model_limit_values(['text/plain', 'text/css', 'text/html', 'application/javascript', 'application/json'], v)
+
+
+class ActionModel(BaseModel):
+    fixed_response: Optional[FixedResponseModel]
+    health_check: Optional[HealthCheckModel]
+    redirect: Optional[RedirectModel]
+    target_port = 8080
+    target_protocol = 'HTTP'
+    target_group_attributes: Dict[str,str] = {}
+    targets: List[TargetModel] = []
+
+    @validator('target_group_attributes', always=True)
+    def include_defaults_in_target_group_attributes(cls, v):
+        return {**{'stickiness.enabled': 'true', 'stickiness.type': 'lb_cookie'}, **v}
+
+
+    @validator('target_protocol')
+    def check_target_protocol(cls, v):
+        return model_limit_value(['HTTP', 'HTTPS'], v)
+
+    @validator('targets', pre=True, each_item=True)
+    def coerce_targets(cls, v):
+        if isinstance(v, str):
+            return TargetModel(id=v)
+        return v
+
+
+class ListenerModel(BaseModel):
+    hostnames: List[HostnameModel] = []
+    protocol: str
+    port: int
+    https_redirect_to: Optional[int]
+    rules: List[RuleModel] = []
+
+    @root_validator(pre=True)
+    def detect_protocol(cls, values):
+        if 'protocol' not in values:
+            port = values['port']
+            if port == 80:
+                values['protocol'] = 'HTTP'
+            else:
+                values['protocol'] = 'HTTPS'
+        return values
+
+    @validator('hostnames', pre=True, each_item=True)
+    def coerce_hostnames(cls, v):
+        if isinstance(v, str):
+            return HostnameModel(hostname=v)
+        return v
+
+    @root_validator
+    def require_hostnames_for_https(cls, values):
+        if values['protocol'] == 'HTTPS' and ('hostnames' not in values or len(values['hostnames']) < 1):
+            raise ValueError('hostnames key is required for HTTPS listeners')
+        return values
+
+
+class UserDataModel(BaseModel):
+    listeners: List[ListenerModel]
+    subnet_ids: List[str]
+    domain: Optional[str]
+    internet_facing: bool
+    access_log_retain_days = 90
+    allow_cidrs: List[str] = []
+    attributes: Dict[str,str] = {}
+    certificate_validation_method = 'DNS'
+    elb_security_groups: List[str] = {}
+    elb_tags: Dict[str,str] = {}
+    hosted_zone_id: Optional[str]
+
+    @root_validator
+    def require_listeners(cls, values):
+        if len(values['listeners']) < 1:
+            raise ValueError('at least one listener is required')
+        return values
+
+    @root_validator
+    def require_subnet_ids(cls, values):
+        if len(values['subnet_ids']) < 2:
+            raise ValueError('at least two subnet_ids are required')
+        return values
+
+    @validator('attributes', always=True)
+    def include_default_attributes(cls, v):
+        return {**{'access_logs.s3.enabled': 'true', 'access_logs.s3.prefix': '${AWS::StackName}-access.'}, **v}
 
 
 TEMPLATE = Template()
